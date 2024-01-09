d8fb2dc9ce1d563af890458679566c9b5833b8df
Trulyola <156133789+Trulyola@users.noreply.github.com>
2024-01-09 20:58:29 +0000

    Add files via upload

---
+++ hangman_parts.py
@@ -0,0 +1,68 @@
+def parts(x):
+    if x == 1:
+        print('|/    ')
+        print('|     ')
+        print('|     ')
+        print('|     ')
+        print('|\    ')
+        print('========')
+    if x == 2:
+        print('------')
+        print('|/   |')
+        print('|     ')
+        print('|     ')
+        print('|     ')
+        print('|\    ')
+        print('========')
+    if x == 3:
+        print('------')
+        print('|/   |')
+        print('|    o')
+        print('|     ')
+        print('|     ')
+        print('|\    ')
+        print('========')
+    if x == 4:
+        print('------')
+        print('|/   |')
+        print('|    o')
+        print('|   -|')
+        print('|     ')
+        print('|\    ')
+        print('========')
+    if x == 5:
+        print('------')
+        print('|/   |')
+        print('|    o')
+        print('|   -|-')
+        print('|     ')
+        print('|\    ')
+        print('========')
+    if x == 6:
+        print('------')
+        print('|/   |')
+        print('|    o')
+        print('|   -|-')
+        print('|    |')
+        print('|\    ')
+        print('========')
+    if x == 7:
+        print('------')
+        print('|/   |')
+        print('|    o')
+        print('|   -|-')
+        print('|    |')
+        print('|\  / ')
+        print('========')
+    if x == 8:
+        print('------')
+        print('|/   |')
+        print('|    o')
+        print('|   -|-')
+        print('|    |')
+        print('|\  / \ ')
+        print('========')
+
+
+
+        
